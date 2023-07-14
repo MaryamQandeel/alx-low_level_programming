17448b53fe2b70fbf034255bd051b469dbcb4a7d
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-07-14 16:01:42 -0700

    fd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - point
+ *
+ * Description: somthing
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putcahr('\n');
+
+	Return(0);
+}
