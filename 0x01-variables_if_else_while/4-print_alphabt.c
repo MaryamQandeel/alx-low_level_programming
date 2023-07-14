d4cd753b60b3c540dba14a1c6e00d88bc06dc723
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-07-14 16:22:53 -0700

    fd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - point
+ *
+ * Description: somthing
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z';
+	{
+		if (ch == 'e' || ch == 'q')
+		ch++
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
