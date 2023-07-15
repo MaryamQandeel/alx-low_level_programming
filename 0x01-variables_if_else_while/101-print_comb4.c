49614a681cb3b9b6e20e4374aade0858984ad1a5
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-07-14 18:18:32 -0700

    r

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - point
+ *
+ * Description: print the digits
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int a = 0, b, c;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			c = 0;
+			while (c <= 9)
+			{
+				if (a < b < c && a != b && a != c && b != c)
+				{
+					putchar(a + 48);
+					putchar(b + 48);
+					putchar(c + 48);
+					if (a + b + c != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			b++;
+		}
+
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
