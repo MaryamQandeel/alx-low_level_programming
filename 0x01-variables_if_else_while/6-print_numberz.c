c92167b37e74fe6a5dc07bc74ca65188eacb0b5b
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-07-14 17:00:57 -0700

    f

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <srdio.h>
+
+/**
+ * main - point
+ *
+ * Description: somthing
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
