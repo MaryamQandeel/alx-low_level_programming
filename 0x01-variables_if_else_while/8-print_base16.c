b44b88fb5bb102526e68923b7be328080c00aa68
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-07-14 17:16:47 -0700

    e

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - point
+ *
+ * Description: print the digits
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit)
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
