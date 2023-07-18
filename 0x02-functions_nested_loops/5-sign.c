c4614dda741e0160a9f9099ab0fdd16ca9d1c4e6
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-07-18 16:29:38 -0700

    as

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(+);
+		_putchar(43);
 		return (1);
 	}
 	if (n == 0)
 	{
-		_putchar(0);
+		_putchar(48);
 		return (0);
 	}
 	if (n < 0)
 	{
-		_puthchar(-);
+		_puthchar(45);
 		return (-1);
 	}
 }
