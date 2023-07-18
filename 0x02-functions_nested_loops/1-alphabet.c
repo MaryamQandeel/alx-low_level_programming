5fa94427469574a9535479b1611ca860c2ed0111
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-07-18 14:48:15 -0700

    r

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - point
+ *
+ * Description: somthing
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		putchar('\n');
+		ch++;
+	}
+
+	return (0);
+}
