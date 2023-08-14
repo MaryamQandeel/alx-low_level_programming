34bf46b8b59540b1140aa37495c96dcad956141c
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-08-14 13:59:11 -0700

    f

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,14 +13,14 @@
  */
 
 
-struct dog *my_dog(char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-	struct dog *my_dog;
-
-	my_dog = malloc(sizeof(struct dog));
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
-	return (my_dog);
+	if (d)
+	{
+	d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
 }
