ad8075009a786bb698d2b218d0c07dd5c1873096
maryamQandeel <maryam.qandeel@YAHOO.COM>
2023-08-14 13:09:12 -0700

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+struct dog *my_dog(char *name, float age, char *owner)
+{
+
+	struct dog *my_dog;
+
+	my_dog = malloc(sizeof(dog));
+	my_dog->name = name;
+	my_dog->age = float;
+	my_dog->owner = owner;
+	return (my_dog);
+}
+#endif /*DOG_h*/
