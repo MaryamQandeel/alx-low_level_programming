1e5e3cf14ac4dab774d1fa9d51470d7d51e6c3e6
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-08-16 22:47:59 +0300

    Create function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef "function_pointers_h"
+#define "function_pointers_h"
+  
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
