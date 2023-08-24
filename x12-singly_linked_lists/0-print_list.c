a269502678c9fc0c4c73cc10260d09450aa8c962
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-08-25 02:52:18 +0300

    tomorrow

--- x12-singly_linked_lists/0-print_list.c
+++ x12-singly_linked_lists/0-print_list.c
@@ -7,31 +7,17 @@
  * Return: result is (0)
  */
 
-int _strlen(char *s)
-{
-	int i;
-	int l = sizeof(s);
-
-	for (i = 0; i < s[l]; i++)
-		if (!s)
-			printf("[0] (nil)\n");
-		return (0);
-	return (i);
-}
-
-/**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- * Return: the number of nodes printed
- */
-
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
 	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
 		s++;
 		h = h->next;
