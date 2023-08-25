938047aa1af6533a1ebb7da902ab3e452229782c
maryamQandeel <maryam.qandeel@yahoo.com>
2023-08-24 17:40:10 -0700

    :r:

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * list_len - function
+ * Return: number of elements
+ * @h: is the string
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int i;
+
+	for (i = 0; i < h; i++)
+		h = h->next;
+	return (i);
+}
