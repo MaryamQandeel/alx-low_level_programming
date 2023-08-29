cca1b1fc89113b227c068759e3278d7559ecc715
maryamQandeel <maryam.qandeel@yahoo.com>
2023-08-29 15:35:44 -0700

    betty by sandbox

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-    size_t nu = 0;
-   
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        nu++;
-    }
-    return (nu);
+	size_t nu = 0;
+
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	nu++;
+	}
+	return (nu);
 }
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,4 +1,4 @@
-more singly linked lists 
+more singly linked lists
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
