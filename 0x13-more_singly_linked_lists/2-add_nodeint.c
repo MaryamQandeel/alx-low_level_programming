c43ac87f0c22e79061a14ea16bcd5b23ead4fbfb
maryamQandeel <maryam.qandeel@yahoo.com>
2023-08-29 16:45:02 -0700

    git file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,37 +1,44 @@
+#include "lists.h"
+
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list.
  * @head: double pointer to the beginning of a listint_t list
  * @n: integer to add to the list
+ * print_listint - prints the element.
  *
  * Return: address of new node
  */
-listint_t *add_nodeint(listint_t **head, const int n);
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *temp;
+	listint_t *temp;
 
-if (head == NULL)
-return (NULL);
-temp = malloc(sizeof(listint_t));
-if (temp == NULL)
-return (NULL);
-temp->n = n;
-temp->next = *head;
-*head = temp;
-return (temp);
+	if (head == NULL)
+	return (NULL);
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	return (NULL);
+	temp->n = n;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }
-
-
-size_t print_listint(const listint_t *h);
+/**
+ * print_listint - adds a new node at the beginning of a listint_t list.
+ * @h: double pointer to the beginning of a listint_t list
+ *
+ * Return: address of new node
+ */
 
 size_t print_listint(const listint_t *h)
 {
-    size_t nu = 0;
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        nu++;
-    }
-    return (nu);
+	size_t nu = 0;
+
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	nu++;
+	}
+	return (nu);
 }
