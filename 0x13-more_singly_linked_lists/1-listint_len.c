74aa65ccd613a94c38c38fa6ea09e2c0513a90a4
maryamQandeel <maryam.qandeel@yahoo.com>
2023-08-29 15:40:10 -0700

    git file

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint -  prints all the elements of a listint_t list.
+ * listint_len -  prints all the elements of a listint_t list.
  * @h: list to print
  * Return: number of elements in the list
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nu = 0;
 
