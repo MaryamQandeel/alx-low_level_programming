12344d4bb862f029d9f6f26565cc0526a1de0558
maryamQandeel <maryam.qandeel@yahoo.com>
2023-08-29 17:10:49 -0700

    git file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - deletes the node at index. 
+ * delete_nodeint_at_index - deletes the node at index.
  * of a listint_t linked list.
  * @head: double pointer to the first node in the list
  * @index: index of the node to delete
