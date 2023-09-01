fa8255252e1f3d40f8cdcad43bb0eef8d6ccdb8f
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-09-01 03:19:54 +0300

    get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the end
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+	char *c;
+
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
+}
