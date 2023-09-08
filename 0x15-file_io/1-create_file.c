c1cbd9b5d22dedf4bc50128903bbde193fdf747f
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-09-08 04:00:57 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	written = write(opened, text_content, i);
 	closed = close(opened);
-  if (closed == -1 || opened == -1 || written == -1 || filename == NULL)
+	if (closed == -1 || opened == -1 || written == -1 || filename == NULL)
 		return (-1);
 	return (1);
 }
