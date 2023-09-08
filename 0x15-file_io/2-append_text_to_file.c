b405ea42e5d00a442a348cfa2a27929a3acd56f3
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-09-08 04:13:48 +0300

    append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,19 +7,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int opened, wrote, len = 0;
+	int opened, written, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (i = 0; text_content[i];)
+			i++;
 	}
 	opened = open(filename, O_WRONLY | O_APPEND);
-	wrote = write(opened, text_content, len);
+	written = write(opened, text_content, i);
 
-	if (opened == -1 || wrote == -1)
+	if (opened == -1 || written == -1)
 		return (-1);
 
 	close(opened);
