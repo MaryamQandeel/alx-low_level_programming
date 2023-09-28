32260c06683734c0ad85e28dbdc8de4385b8588c
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-09-29 02:43:01 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef BIT_MANIPULATION_H
 #define BIT_MANIPULATION_H
+#include <stddef.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
