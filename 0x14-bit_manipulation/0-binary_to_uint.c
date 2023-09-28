21fb62aaa15445edfa379c83874fcb1808845bd6
maryamQndeel <maryam.qandeel@yahoo.com>
2023-09-28 16:38:30 -0700

    fix betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,11 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int n;
-	n = 0;
-  
-  if (b == NULL)
-    return (0);
-  for (i = 0; b[i] != '\0'; i++)
+	unsigned int n = 0;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
