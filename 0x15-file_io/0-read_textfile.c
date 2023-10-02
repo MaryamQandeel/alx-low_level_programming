287d8f87cb98d24b80f860fd9333a009e3dcfbf3
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-10-03 00:19:31 +0300

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-    /**
-    * read_textfile isa function - returns the actual number of letters it could read and print
-    * @filename: file to be read
-    * @letters: the number of letters it should read and print
-    * 
-    * Return: number bytes read/print
-    */
+/**
+* read_textfile isa function - returns the actual number of letters it could read and print
+* @filename: file to be read
+* @letters: the number of letters it should read and print
+* 
+* Return: number bytes read/print
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
