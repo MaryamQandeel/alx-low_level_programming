90050cf972356457c0026f404a19d41c1d7db7c5
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-10-03 00:14:21 +0300

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-    /*
+    /**
     * read_textfile isa function - returns the actual number of letters it could read and print
-    *
     * @filename: file to be read
     * @letters: the number of letters it should read and print
     * 
-    * return 0 if filename is NULL, can not be opened or read
     * Return: number bytes read/print
-    **/
+    */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
     ssize_t bytes;
     char c;
     int openFile = open(filename, O_RDONLY);
@@ -19,6 +18,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
     bytes = write(STDOUT_FILENO, &c, bytes);
     if (filename == NULL | openFile == -1 | !letters)
         return (0);
-        close(openFile);
-        return (bytes);
+    close(openFile);
+    return (bytes);
 }
